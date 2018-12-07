7ba23c7cd7d191d4fae281b42fd4b17180bed390
Simon grahn <simon.grahn@gmail.com>
2018-12-07 12:39:03 +0100

    AJAX - can get a key. but gets whole message. needs to narrow it down

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 $(document).ready(() => {
+  const $bookList = $('.book-list');
+  const $listenButton = $('#listenButton');
+  const $outputDivKey = $('#receviedKey');
+  const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey';
+  const settings = {
+    method: 'GET',
+    data: {
+      status: 'success',
+      key: 'value',
+    },
+  };
 
-console.log('Hello world');
+
+  $listenButton.on('click', event => {
+    $.ajax(url, settings)
+      .done(whenAjaxDone).fail(whenAjaxFails).always(onSucces);
+  });
+
+  function whenAjaxDone(data) {
+    let newData = data.key;
+    $outputDivKey.append($('#receviedKey').text(data));
+    console.log('Server returned!');
+  };
+
+  function whenAjaxFails(data) {
+    console.log('Does not work');
+    alert('does not work');
+  };
+
+  function onSucces(data) {
+    console.log('server send!');
+  };
+
+
+
+
+
+
 
 
 

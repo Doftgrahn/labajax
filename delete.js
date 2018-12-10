d7fe89192e534095151d47ede398dad76369a5f7
Simon grahn <simon.grahn@gmail.com>
2018-12-10 16:44:16 +0100

    DONE

--- delete.js
+++ delete.js
@@ -1,4 +1,34 @@
 $(document).ready(() => {
+  let deleteButton = $('#deletebook');
+
+
+  deleteButton.on('click', event => {
+
+      let deleteID = ('#deleteID');
+
+
+    let url = 'https://www.forverkliga.se/JavaScript/api/crud.php';
+    let settings = {
+      method: 'GET',
+      data: {
+        op: 'delete',
+        key: apiKey,
+        id: $('#deleteID').val(),
+      },
+    };
+
+    $.ajax(url, settings)
+      .done(data => {
+        console.log('deleted');
+      })
+      .fail(error => {
+        console.log('error');
+      })
+      .always(always => {
+        console.log(always);
+      })
+
+  });
 
 
 
